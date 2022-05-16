7658d2c9499d04237ea1b6bfead312d62130405d
iamatnuria <ignasiamat10@gmail.com>
2022-05-16 21:56:25 +0200

    testing done & working, front end pending

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">Metaverse Marketplace</p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a href="" className="mr-4 text-pink-500">Home</a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
